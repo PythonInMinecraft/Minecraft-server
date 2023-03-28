46f208c02c707f6f9982d8c04dbfb64071e8b99e
FewerElk <gabriel@trouilhet.fr>
2023-03-28 16:04:28 +0200

    DOCUMENTATION

---
+++ Server/main.py
@@ -0,0 +1,14 @@
+"""A Minecraft server in python 3"""
+from socket import *
+from time import *
+
+if __name__ == "__main__":
+    print("""
+    _________________________________________________
+                Minecraft server in Python 3
+                    Created by FewerElk
+                   (github.com/FewerElk)
+    If you have an issue please report it in GitHub !
+    _________________________________________________
+    """)
+    print("Launching Minecraft server...")
