5eca62fd0e302156a2d2e22b21111475820d3b29
FewerElk <gabriel@trouilhet.fr>
2023-04-03 18:27:33 +0200

    Save game code is created

--- Server/classes/save_game.py
+++ Server/classes/save_game.py
@@ -2,6 +2,8 @@
 
 class Save(object):
     """Save the game"""
+    BLOCKS = {"minecraft:air":0, "minecraft:grass_block":1, "minecraft:dirt":2, "minecraft:stone":3, "minecraft:cobble_stone":4}
+
     def __init__(self, file, data):
         """Save the game"""
         with open(file, "w") as file:
@@ -9,3 +11,15 @@ class Save(object):
 
     def encode(self, data):
         """Encode datas chunks / chuncks"""
+        final = ""
+        for chunk in self.data:
+            #Save a chunk (16/16/16 blocks)
+            c = chunk[0]        #C is a dictionnary : c is like this : {"x":00, "y":00, "z":00}
+            final += "{0};{1};{2}{".format(c["x"], c["y"], c["z"])
+            for block in chunk[1]:
+                #Save a block
+                final += self.BLOCKS[block]
+                final += ";"
+            final = final[:-1]
+            final += "};"
+        return None
--- Server/classes/server/format.md
+++ Server/classes/server/format.md
@@ -1,7 +1,12 @@
 # Save format
-<chunk coord>{<hexa value for a block><hexa value for a block><hexa value for a block>...}
---> blocks starts from upside x=0 y=0
+<chunk coord exa>{<hexa value for a block><hexa value for a block><hexa value for a block>...}
+--> example :
+a5;2b;10{00;00;00;00;00;00;00;00;01;01;01;01;01;02;03...};g2;00;23{40;60;65;01;v5...}
+--> blocks starts from upside x=0 y=0 z=0
 
 # Blocks id
 - grass_block : 1
 - air : 0
+- dirt : 2
+- stone : 3
+- cobble_stone : 4
