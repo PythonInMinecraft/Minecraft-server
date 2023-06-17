06057cce68d736b7dd3dae0e559e7b2c292e4ed5
FewerElk <120582916+FewerElk@users.noreply.github.com>
2023-06-17 16:39:50 +0200

    Update generator.py

--- Server/classes/filing/generator.py
+++ Server/classes/filing/generator.py
@@ -1,10 +1,18 @@
 """The Generator class"""
 
 class Generator(object):
-    def void_generator(self):
+    def void_generator(self, type="o"):
         """Return a void world"""
+        if type == "o":
+            mat = "minecraft:stone"
+        elif type == "n":
+            mat = "minecraft:cobble_stone"
+        elif type == "e":
+            mat = "minecraft:dirt"
+        else:
+            mat = "minecraft:stone"
         chunk = []
         for i in range(16*16*16):
             #Repeat for every blocs of the chunk
-            chunk.append("minecraft:stone")
+            chunk.append(mat)
         return [({"x":0, "y":0, "z":0}, chunk)]
